590b7cb014f9f92587ba2583ab31b75aa6824378
omprakash <omprakashsridharan@outlook.com>
2017-10-27 18:43:51 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  endTime = new Date('August 19, 2018 18:00:00 PDT').getTime();
+  days;
+  hours;
+  minutes;
+  seconds;
+  constructor() {
+    const x = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = this.endTime - now;
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      if (distance <= 0) {
+        clearInterval(x);
+      }
+    }, 1000);
+  }
 }
